44f6ca454fd2bf5dab3c04f819fa241162b9fabd
Ken <67538811+YinHk@users.noreply.github.com>
2022-12-18 15:28:52 -0500

    Create console.cs

---
+++ cheatsheet/system/console.cs
@@ -0,0 +1,18 @@
+// public static class Console
+// Represents the standard input, output, and error streams for console applications. 
+// This class cannot be inherited.
+
+using System;
+
+// Writes the specified data, followed by the current line terminator, to the standard output stream.
+Console.WriteLine();
+
+// Writes the text representation of the specified value or values to the standard output stream
+Console.Write();
+
+
+/*
+  The difference between Write(…) and WriteLine(…) is that the Write(…) method prints on the console what it is provided between the parentheses but does nothing in addition while the method WriteLine(…) means directly “write line”. 
+  This method does what the Write(…) one does but in addition goes to a new line.
+ */    
+
