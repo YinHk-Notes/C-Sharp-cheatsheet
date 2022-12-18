5c5398ad95d2f8095570fef1b49fc7d5d3c91377
Ken <67538811+YinHk@users.noreply.github.com>
2022-12-18 15:34:09 -0500

    Update console.cs

--- cheatsheet/system/console.cs
+++ cheatsheet/system/console.cs
@@ -10,7 +10,6 @@ Console.WriteLine();
 // Writes the text representation of the specified value or values to the standard output stream
 Console.Write();
 
-
 /*
   The difference between Write(…) and WriteLine(…) is that the Write(…) method prints on the console what it is provided between the parentheses but does nothing in addition while the method WriteLine(…) means directly “write line”. 
   This method does what the Write(…) one does but in addition goes to a new line.
@@ -24,4 +23,7 @@ Console.Write();
   I love
   this Book!  //same line
  */    
+
+// Clears the console buffer and corresponding console window of display information
+Console.Clear();
 
