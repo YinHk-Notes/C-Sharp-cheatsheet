0f6161f9c339c7b111f11b39751a7a45150b4a72
Ken <67538811+YinHk@users.noreply.github.com>
2022-12-18 15:47:29 -0500

    Update console.cs

--- cheatsheet/system/console.cs
+++ cheatsheet/system/console.cs
@@ -34,5 +34,37 @@ Console.Read();
 // Read the next line of characters from the standard input stream
 Console.ReadLine();
 
+/* 
+eg:
+public static void Main()
+    {
+        int age;
+        string name;
+ 
+        Console.WriteLine("Enter your name: ");
+         
+        // using the method
+        // typecasting not needed
+        // as ReadLine returns string
+        name = Console.ReadLine();
+         
+        Console.WriteLine("Enter your age: ");
+         
+        // Converted string to int
+        age = Convert.ToInt32(Console.ReadLine());
+         
+        if (age >= 18)
+        {
+            Console.WriteLine("Hello " + name + "!"
+                        + " You can vote");
+        }
+        else {
+            Console.WriteLine("Hello " + name + "!"
+                + " Sorry you can't vote");
+        }
+    }
+*/
 
+
+}
 
