b6d77aa93fbe92d50dfefd3da013b97b2ef67200
Ken <67538811+YinHk@users.noreply.github.com>
2023-01-01 02:30:56 -0500

    Create ArrayList.cs

---
+++ cheatsheet/System.Collections/ArrayList.cs
@@ -0,0 +1,12 @@
+// Implements the IList interface using an array whose size is dynamically increased as required.
+// Inheritance: Object -> ArrayList
+// Derived: System.Windows.Forms.DomainUpDown.DomainUpDownItemCollection
+// Implements: ICollection, IEnumerable, IList, ICloneable
+
+
+// Declare an ArrayList
+ArrayList al = new ArrayList();   
+ArrayList al = new ArrayList(ICollection);  //contains elements copied from the specified collection and that has the same initial capacity as the number of elements copied.
+
+
+
