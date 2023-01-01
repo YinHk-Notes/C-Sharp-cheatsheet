0188a4a2c5bb98941f0dfcf18b0dc8702977b5ef
Ken <67538811+YinHk@users.noreply.github.com>
2023-01-01 16:38:09 -0500

    Update ArrayList.cs

--- cheatsheet/System.Collections/ArrayList.cs
+++ cheatsheet/System.Collections/ArrayList.cs
@@ -10,8 +10,8 @@ ArrayList list = new ArrayList();
 ArrayList list = new ArrayList(ICollection);  //contains elements copied from the specified collection and that has the same initial capacity as the number of elements copied.
 
 
-//Add an element
-list.Add(e);
+//Add an object o to the end of of the ArrayList
+list.Add(Object o);
 
 //Add the elements of an ICollection c to the end of the ArrayList.
 list.(ICollection c);
