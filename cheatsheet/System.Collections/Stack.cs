0d1992876a1c67f1ba73d9748549a0328e61341e
Ken <67538811+YinHk@users.noreply.github.com>
2023-01-01 22:55:07 -0500

    Update Stack.cs

--- cheatsheet/System.Collections/Stack.cs
+++ cheatsheet/System.Collections/Stack.cs
@@ -6,9 +6,10 @@ using System.Collections;
 
 public class Stack : ICloneable, System.Collections.ICollection
 
-
-
-
+//Declare Stack
+Stack s = new Stack();   //empty and has the default initial capacity.
+Stack s = new Stack(System.Collections.ICollection col); //contains elements copied from the specified collection with the same initial capacity
+Stack s = new Stack(int initialCapacity);  //empty and has the specific capacity.
 
 
 
