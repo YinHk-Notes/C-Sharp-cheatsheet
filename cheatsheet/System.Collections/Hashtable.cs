bbcc2358db750ecb0fdf291b0120dcb328abb9b4
Ken <67538811+YinHk@users.noreply.github.com>
2023-01-01 23:38:01 -0500

    Create Hashtable.cs

---
+++ cheatsheet/System.Collections/Hashtable.cs
@@ -0,0 +1,13 @@
+//Represents a collection of key/value pairs that are organized based on the hash code of the key.
+//Inheritance: Object => Hashtable
+//Derived: System.Configuration.SettingsAttributeDictionary, System.Configuration.SettingsContext, System.Data.PropertyCollection, System.Printing.IndexedProperties.PrintPropertyDictionary
+//Implements: ICollection, IDictionary, IEnumerable, ICloneable, IDeserializationCallback, ISerializable
+
+using System.Collections;
+
+public class Hashtable : ICloneable, System.Collections.IDictionary, System.Runtime.Serialization.IDeserializationCallback, System.Runtime.Serialization.ISerializable
+
+
+
+
+
