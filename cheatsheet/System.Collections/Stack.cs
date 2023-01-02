e05349593f5861228a536d1f735e0fe408706306
Ken <67538811+YinHk@users.noreply.github.com>
2023-01-01 22:41:11 -0500

    Create Stack.cs

---
+++ cheatsheet/System.Collections/Stack.cs
@@ -0,0 +1,17 @@
+//Represents a simple last-in-first-out (LIFO) non-generic collection of objects.
+//Implements ICollection, IEnumerable, ICloneable
+//Inheritance: Object -> Stack
+
+using System.Collections;
+
+public class Stack : ICloneable, System.Collections.ICollection
+
+
+
+
+
+
+
+
+
+
