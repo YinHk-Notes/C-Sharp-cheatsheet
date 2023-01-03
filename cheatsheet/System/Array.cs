7dd8640dfe86b92845ce4551b97f048add465fa3
Ken <67538811+YinHk@users.noreply.github.com>
2023-01-02 22:23:29 -0500

    Create Array.cs

---
+++ cheatsheet/System/Array.cs
@@ -0,0 +1,48 @@
+//Provides methods for creating, manipulating, searching, and sorting arrays
+//Inheritance: Object -> Array
+//Implements: ICollection, IEnumerable, IList, IStructuralComparable, IStructuralEquatable, ICloneable
+
+using System;
+
+public abstract class Array : ICloneable, System.Collections.IList, System.Collections.IStructuralComparable, System.Collections.IStructuralEquatable
+
+
+//Properties
+public bool IsFixedSize { get; }     //Gets a value indicating whether the Array has a fixed size.
+public bool IsReadOnly { get; }      //Gets a value indicating whether the Array is read-only.
+public bool IsSynchronized { get; }  //Gets a value indicating whether access to the Array is synchronized.
+public int Length { get; }           //Gets the total number of elements in all the dimensions of the Array.
+public long LongLength { get; }      //Gets a 64-bit integer that represents the total number of elements in all the dimensions of the Array.
+public static int MaxLength { get; } //Gets the maximum number of elements that may be contained in an array.
+public int Rank { get; }             //Gets the rank (number of dimensions) of the Array.
+
+
+//Returns a read-only wrapper for the specified array.
+public static System.Collections.ObjectModel.ReadOnlyCollection<T> AsReadOnly<T> (T[] array);
+
+//Searches a one-dimensional sorted Array for a value, using a binary search algorithm.
+public static int BinarySearch (Array array, object? value);
+public static int BinarySearch (Array array, object? value, System.Collections.IComparer? comparer); //using the specified IComparer interface
+public static int BinarySearch (Array array, int index, int length, object? value); //index: starting, length: The length of the range to search.
+public static int BinarySearch (Array array, int index, int length, object? value, System.Collections.IComparer? comparer);
+public static int BinarySearch (Array array, int index, int length, object? value, System.Collections.IComparer? comparer);
+public static int BinarySearch<T> (T[] array, T value, System.Collections.Generic.IComparer<T>? comparer);
+public static int BinarySearch<T> (T[] array, int index, int length, T value);
+public static int BinarySearch<T> (T[] array, int index, int length, T value, System.Collections.Generic.IComparer<T>? comparer);
+
+//Clears the contents of an array.
+public static void Clear (Array array);
+
+//Creates a shallow copy of the Array.
+public object Clone ();
+
+//opies a range of elements from an Array starting at the specified source index and pastes them to another Array starting at the specified destination index. Guarantees that all changes are undone if the copy does not succeed completely.
+public static void ConstrainedCopy (Array sourceArray, int sourceIndex, Array destinationArray, int destinationIndex, int length);
+
+
+
+
+
+
+
+
