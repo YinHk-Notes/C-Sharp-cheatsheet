bb60bc10ec99c192fc1fb0a1e80b51e6eccb2532
Ken <67538811+YinHk@users.noreply.github.com>
2023-01-02 21:38:48 -0500

    Update SortedList.cs

--- cheatsheet/System.Collections/SortedList.cs
+++ cheatsheet/System.Collections/SortedList.cs
@@ -15,3 +15,27 @@ SortedList list = new SortedList(System.Collections.IComparer comparer, int init
 SortedList list = new SortedList(System.Collections.IDictionary d, System.Collections.IComparer comparer);
 
 
+//Properties
+public virtual int Capacity { get; set; }   //Gets or sets the capacity of a SortedList object.
+public virtual int Count { get; }           //Gets the number of elements contained in a SortedList object.
+public virtual bool IsFixedSize { get; }    //Gets a value indicating whether a SortedList object has a fixed size.
+public virtual bool IsReadOnly { get; }
+public virtual bool IsSynchronized { get; }
+public virtual object? this[object key] { get; set; }         //Gets or sets the value associated with a specific key in a SortedList object.
+public virtual System.Collections.ICollection Keys { get; }   //Gets the keys in a SortedList object.
+public virtual System.Collections.ICollection Values { get; } //Gets the values in a SortedList object.
+
+
+//Adds an element with the specified key and value to a SortedList object.
+public virtual void Add (object key, object? value);
+
+//Removes all elements from a SortedList object.
+public virtual void Clear ();
+
+
+
+
+
+
+
+
