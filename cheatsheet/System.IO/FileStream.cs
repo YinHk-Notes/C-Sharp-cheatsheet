0185406623ff062f1d28fe52080a71f533662d23
Ken <67538811+YinHk@users.noreply.github.com>
2023-08-01 06:30:45 -0400

    Update FileStream.cs

--- cheatsheet/System.IO/FileStream.cs
+++ cheatsheet/System.IO/FileStream.cs
@@ -9,7 +9,7 @@ using System.IO;
 public class FileStream : System.IO.Stream
 
 
-//constructors
+//Constructors
 public FileStream (Microsoft.Win32.SafeHandles.SafeFileHandle handle, System.IO.FileAccess access);
 public FileStream (Microsoft.Win32.SafeHandles.SafeFileHandle handle, System.IO.FileAccess access, int bufferSize);
 public FileStream (Microsoft.Win32.SafeHandles.SafeFileHandle handle, System.IO.FileAccess access, int bufferSize, bool isAsync);
@@ -34,6 +34,30 @@ public override long Position { get; set; }   //Gets or sets the current positio
 public virtual int ReadTimeout { get; set; }  //Gets or sets a value, in milliseconds, that determines how long the stream will attempt to read before timing out.
 public virtual Microsoft.Win32.SafeHandles.SafeFileHandle SafeFileHandle { get; } //Gets a SafeFileHandle object that represents the operating system file handle for the file
 public virtual int WriteTimeout { get; set; }  //Gets or sets a value, in milliseconds, that determines how long the stream will attempt to write before timing out.
+
+//Methods
+public virtual void Close (); //Closes the current stream and releases any resources associated with the current stream.
+public void CopyTo (System.IO.Stream destination); //Reads the bytes from the current stream and writes them to another stream.
+public virtual void CopyTo (System.IO.Stream destination, int bufferSize); //Reads the bytes from the current stream and writes them to another stream, using a specified buffer size. 
+public System.Threading.Tasks.Task CopyToAsync (System.IO.Stream destination); //Asynchronously reads the bytes from the current stream and writes them to another stream.
+public System.Threading.Tasks.Task CopyToAsync (System.IO.Stream destination, System.Threading.CancellationToken cancellationToken);
+public System.Threading.Tasks.Task CopyToAsync (System.IO.Stream destination, int bufferSize);
+public override System.Threading.Tasks.Task CopyToAsync (System.IO.Stream destination, int bufferSize, System.Threading.CancellationToken cancellationToken);
+[System.Security.SecurityCritical]
+public virtual System.Runtime.Remoting.ObjRef CreateObjRef (Type requestedType); //Creates an object that contains all the relevant information required to generate a proxy used to communicate with a remote object.
+public void Dispose ();  //Releases all resources used by the Stream.
+public override int EndRead (IAsyncResult asyncResult); //Waits for the pending asynchronous read operation to complete. 
+public override void EndWrite (IAsyncResult asyncResult); //Ends an asynchronous write operation and blocks until the I/O operation is complete.
+
+public override int ReadByte (); //Reads a byte from the file and advances the read position one byte.
+
+
+
+
+
+
+
+
 
 
 
