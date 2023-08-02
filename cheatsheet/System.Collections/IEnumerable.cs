756df1fdcc8b04430885a3048423e79e4f085fb4
Ken <67538811+YinHk@users.noreply.github.com>
2023-08-01 20:46:54 -0400

    Create IEnumerable.cs

---
+++ cheatsheet/System.Collections/IEnumerable.cs
@@ -0,0 +1,13 @@
+//IEnumerable
+//Exposes an enumerator, which supports a simple iteration over a non-generic collection.
+
+
+using System.Collections;
+
+public interface IEnumerable
+
+
+//Methods
+public System.Collections.IEnumerator GetEnumerator ();     // Returns an enumerator that iterates through a collection
+
+  
