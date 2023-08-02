93031f348df0af1ca7d97096d38111078b4eff60
Ken <67538811+YinHk@users.noreply.github.com>
2023-08-01 20:43:55 -0400

    Create IEnumerator.cs

---
+++ cheatsheet/System.Collections/IEnumerator.cs
@@ -0,0 +1,192 @@
+//IEnumerator
+//Supports a simple iteration over a non-generic collection.
+
+Using System.Collections;
+
+public interface IEnumerator
+
+//Properties
+public object Current { get; }    // Gets the element in the collection at the current position of the enumerator
+
+
+//Methods
+public bool MoveNext ();          // Advances the enumerator to the next element of the collection
+public void Reset ();             // Sets the enumerator to its initial position, which is before the first element in the collection
+
+
+
+
+
+
+
+//Example;
+using System;
+using System.Collections;
+
+// Simple business object.
+public class Person
+{
+    public Person(string fName, string lName)
+    {
+        this.firstName = fName;
+        this.lastName = lName;
+    }
+
+    public string firstName;
+    public string lastName;
+}
+
+// Collection of Person objects. This class
+// implements IEnumerable so that it can be used
+// with ForEach syntax.
+public class People : IEnumerable
+{
+    private Person[] _people;
+    public People(Person[] pArray)
+    {
+        _people = new Person[pArray.Length];
+
+        for (int i = 0; i < pArray.Length; i++)
+        {
+            _people[i] = pArray[i];
+        }
+    }
+
+// Implementation for the GetEnumerator method.
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+       return (IEnumerator) GetEnumerator();
+    }
+
+    public PeopleEnum GetEnumerator()
+    {
+        return new PeopleEnum(_people);
+    }
+}
+
+// When you implement IEnumerable, you must also implement IEnumerator.
+public class PeopleEnum : IEnumerator
+{
+    public Person[] _people;
+
+    // Enumerators are positioned before the first element
+    // until the first MoveNext() call.
+    int position = -1;
+
+    public PeopleEnum(Person[] list)
+    {
+        _people = list;
+    }
+
+    public bool MoveNext()
+    {
+        position++;
+        return (position < _people.Length);
+    }
+
+    public void Reset()
+    {
+        position = -1;
+    }
+
+    object IEnumerator.Current
+    {
+        get
+        {
+            return Current;
+        }
+    }
+
+    public Person Current
+    {
+        get
+        {
+            try
+            {
+                return _people[position];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
+
+class App
+{
+    static void Main()
+    {
+        Person[] peopleArray = new Person[3]
+        {
+            new Person("John", "Smith"),
+            new Person("Jim", "Johnson"),
+            new Person("Sue", "Rabon"),
+        };
+
+        People peopleList = new People(peopleArray);
+        foreach (Person p in peopleList)
+            Console.WriteLine(p.firstName + " " + p.lastName);
+    }
+}
+
+/* This code produces output similar to the following:
+ *
+ * John Smith
+ * Jim Johnson
+ * Sue Rabon
+ *
+ */
+
+
+
+// When you implement IEnumerable, you must also implement IEnumerator.
+public class PeopleEnum : IEnumerator
+{
+    public Person[] _people;
+
+    // Enumerators are positioned before the first element
+    // until the first MoveNext() call.
+    int position = -1;
+
+    public PeopleEnum(Person[] list)
+    {
+        _people = list;
+    }
+
+    public bool MoveNext()
+    {
+        position++;
+        return (position < _people.Length);
+    }
+
+    public void Reset()
+    {
+        position = -1;
+    }
+
+    object IEnumerator.Current
+    {
+        get
+        {
+            return Current;
+        }
+    }
+
+    public Person Current
+    {
+        get
+        {
+            try
+            {
+                return _people[position];
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
+
+
