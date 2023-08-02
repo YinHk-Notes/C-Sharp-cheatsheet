8ad7f78b36e30d122f1a8970599f490588859b91
Ken <67538811+YinHk@users.noreply.github.com>
2023-08-02 01:36:30 -0400

    Create Dictionary.cs

---
+++ cheatsheet/System.Collections.Generic/Dictionary.cs
@@ -0,0 +1,57 @@
+//Dictionary<TKey,TValue>
+//Represents a collection of keys and values.
+
+using System.Collections.Generic;
+
+public class Dictionary<TKey,TValue> : System.Collections.Generic.ICollection<System.Collections.Generic.KeyValuePair<TKey,TValue>>, 
+  System.Collections.Generic.IDictionary<TKey,TValue>, System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<TKey,TValue>>, 
+    System.Collections.Generic.IReadOnlyCollection<System.Collections.Generic.KeyValuePair<TKey,TValue>>, System.Collections.Generic.IReadOnlyDictionary<TKey,TValue>, 
+      System.Collections.IDictionary, System.Runtime.Serialization.IDeserializationCallback, System.Runtime.Serialization.ISerializable
+
+
+//Constructors
+public Dictionary ();
+public Dictionary (System.Collections.Generic.IDictionary<TKey,TValue> dictionary); // contains elements copied from the specified IDictionary<TKey,TValue>
+//Initializes a new instance of the Dictionary<TKey,TValue> class that contains elements copied from the specified IEnumerable<T>.
+public Dictionary (System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<TKey,TValue>> collection);
+//Initializes a new instance of the Dictionary<TKey,TValue> class that is empty, has the default initial capacity, and uses the specified IEqualityComparer<T>.
+public Dictionary (System.Collections.Generic.IEqualityComparer<TKey>? comparer);
+
+
+//Declare dictionary
+Dictionary<TKey,TValue> dictionary = new Dictionary<TKey,TValue>();
+
+//Properties
+public System.Collections.Generic.IEqualityComparer<TKey> Comparer { get; } //Gets the IEqualityComparer<T> that is used to determine equality of keys for the dictionary.
+public int Count { get; }     //Gets the number of key/value pairs contained
+public TValue this[TKey key] { get; set; }  //Gets or sets the value associated with the specified key
+public System.Collections.Generic.Dictionary<TKey,TValue>.KeyCollection Keys { get; } //Gets a collection containing the keys
+public System.Collections.Generic.Dictionary<TKey,TValue>.ValueCollection Values { get; } //Gets a collection containing the values
+
+
+//Methods
+
+//Adds the specified key and value to the dictionary.
+public void Add (TKey key, TValue value);
+
+//Removes all keys and values 
+public void Clear ();
+
+//Determines whether the Dictionary<TKey,TValue> contains the specified key.
+public bool ContainsKey (TKey key);
+
+//Determines whether the Dictionary<TKey,TValue> contains a specific value.
+public bool ContainsValue (TValue value);
+
+
+
+
+
+
+
+
+
+
+
+
+
