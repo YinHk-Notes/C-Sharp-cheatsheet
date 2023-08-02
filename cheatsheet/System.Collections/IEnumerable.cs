cfc1ce7e77381b0be26a0d96ef045d64f6ec5d00
Ken <67538811+YinHk@users.noreply.github.com>
2023-08-01 21:08:49 -0400

    Update IEnumerable.cs

--- cheatsheet/System.Collections/IEnumerable.cs
+++ cheatsheet/System.Collections/IEnumerable.cs
@@ -8,6 +8,22 @@ public interface IEnumerable
 
 
 //Methods
-public System.Collections.IEnumerator GetEnumerator ();     // Returns an enumerator that iterates through a collection
 
+//// <returns> Returns an enumerator that iterates through a collection </returns>
+public System.Collections.IEnumerator GetEnumerator ();     
+
+/// <summary> Casts the elements of an IEnumerable to the specified type </summary>
+/// <param name="TResult"/> The type to cast the elements of source to </param>
+/// <param name="source"/> The IEnumerable that contains the elements to be cast to type TResult. </param>
+public static System.Collections.Generic.IEnumerable<TResult> Cast<TResult> (this System.Collections.IEnumerable source);
   
+/// <summary> Filters the elements of an IEnumerable based on a specified type </summary>
+/// <param name="TResult"/> The type to filter the elements of the sequence on </param>
+/// <param name="source"/> The IEnumerable whose elements to filter </param>
+public static System.Collections.Generic.IEnumerable<TResult> OfType<TResult> (this System.Collections.IEnumerable source);
+
+
+
+
+
+
