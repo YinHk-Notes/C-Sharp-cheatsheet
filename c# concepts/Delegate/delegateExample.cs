2baf8811b0419586539c66dd24546a2a5ee9ed9c
Ken <67538811+YinHk@users.noreply.github.com>
2024-01-10 16:30:03 -0500

    Update delegateExample.cs

--- c# concepts/Delegate/delegateExample.cs
+++ c# concepts/Delegate/delegateExample.cs
@@ -12,15 +12,21 @@ class Script
 		//delegate 
 		MessageDelegate del = MyDelegate.Message;
 		del.Invoke("This is a simple example of delegate");
+		
 		//Multicast delegate
 		del += MyDelegate.AnotherMessage;
 		del.Invoke("delegate");
+		
 		//Anonymous method in delegate
 		MessageDelegate del2 = delegate (string message)
 		{
 			Console.WriteLine($"{message}");
 		};
 		del2.Invoke("This anonymous");
+		
+		//Action delegate - without care about declaration of delegate
+		Action<string> del3 = MyDelegate.Message;
+		del3.Invoke("This is from Action delegate");
 	}
 }
 
