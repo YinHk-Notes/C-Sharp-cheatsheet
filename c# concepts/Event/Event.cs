3fa8bc057f97e1aa6125dcd03509e931d333a79e
Ken <67538811+YinHk@users.noreply.github.com>
2024-01-12 05:16:28 -0500

    Create Event.cs

---
+++ c# concepts/Event/Event.cs
@@ -0,0 +1,37 @@
+class Script
+{ 
+	static void Main(string[] args)
+	{
+		//event
+		Publisher pub = new Publisher();
+		Subscriber sub = new Subscriber(pub);
+		pub.onAlert();
+
+	}
+}
+  
+class Publisher
+{
+	//C# event
+	public delegate void Alert();
+
+	public event Alert alert;
+
+	//raising an event here
+	public void onAlert()
+	{
+		alert?.Invoke(); //invoke delegate
+	}
+}
+
+class Subscriber
+{
+	public Subscriber(Publisher p) => p.alert += eventHandler;
+
+	public void eventHandler()
+	{
+		Console.Beep(3000, 1000);
+		Console.WriteLine("This is an alert!");
+	}
+}
+
